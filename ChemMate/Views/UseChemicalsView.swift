fc5d0c617b1250d4515dbc6081e596c6053a2d0d
Sean Starling <seanestarling@gmail.com>
2021-06-13 12:31:49 +1000

    Added Use Chemical Rectangle buttons.  need to be changed to active buttons

--- ChemMate/Views/UseChemicalsView.swift
+++ ChemMate/Views/UseChemicalsView.swift
@@ -35,63 +35,125 @@ struct UseChemicalsView: View {
             
             
             VStack{
-            
+                
                 Spacer()
                 
-            // Select the Chemical to be used
-            
-            Text("Chemical Selection")
-                .font(.title2)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            
-                Spacer()
-            
-            // Select Calulation Method
-            Text("Calculation Method")
-                .font(.title2)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            
+                // Select the Chemical to be used
+                
+                Text("Chemical Selection")
+                    .font(.title2)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.bottom, 2.0)
+                
+                Text(" Drop down list or QR Code?")
+                
+                
                 Spacer()
                 
-            // Use  - Date and Amount
-            Text("Use: Date & Amount")
-                .font(.title2)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            
-    
-            
-            VStack{
+                // Select Calulation Method
+                Text("Calculation Method")
+                    .font(.title2)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.bottom, 2.0)
                 
+                Text(" Manual Entry or Scan Scales?")
                 
-                HStack{
-                    Text("Date:")
-                    Text("DD:MM:YYYY")
+                Spacer()
+                
+                // Use  - Date and Amount
+                Text("Use: Date & Amount")
+                    .font(.title2)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.bottom, 2.0)
+                
+                VStack(alignment: .leading){
+                    
+                    
+                    HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                        Text("Date:")
+                        Text("DD:MM:YYYY")
+                    })
+                    
+                    HStack{
+                        Text("Start Weight:")
+                        Text("XXX Kg/L")
+                    }
+                    
+                    HStack{
+                        Text("End Weight:")
+                        Text("XXX Kg/L")
+                    }
+                    
+                    HStack{
+                        Text("Usage:")
+                        Text("XXX Kg/L")
+                    }
+             
                 }
                 
-                HStack{
-                    Text("Start Weight:")
-                    Text("XXX Kg/L")
-                }
-                
-                HStack{
-                    Text("End Weight:")
-                    Text("XXX Kg/L")
-                }
-                
-                HStack{
-                    Text("Usage:")
-                    Text("XXX Kg/L")
-                }
+                Spacer()
                 
             }
+            
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+            
+                    ZStack(alignment: /*@START_MENU_TOKEN@*/Alignment(horizontal: .center, vertical: .center)/*@END_MENU_TOKEN@*/, content: {
+                    
+                            Rectangle()
+                                .foregroundColor(.orange)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .aspectRatio(CGSize(width: 100, height: 30), contentMode: .fit)
+                            .padding(.vertical, 5.0)
+                        
+                            Text("Edit")
+                    })
+                    
+                    ZStack(alignment: /*@START_MENU_TOKEN@*/Alignment(horizontal: .center, vertical: .center)/*@END_MENU_TOKEN@*/, content: {
+                            Rectangle()
+                                .foregroundColor(.green)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .aspectRatio(CGSize(width: 100, height: 30), contentMode: .fit)
+                            .padding(.vertical, 5.0)
+                        
+                            Text("Save")
+                    })
+                    
+                })
+            Spacer()
                 
-                Spacer()
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                ZStack(alignment: /*@START_MENU_TOKEN@*/Alignment(horizontal: .center, vertical: .center)/*@END_MENU_TOKEN@*/, content: {
                 
-            }
+                        
+                        Rectangle()
+                            .foregroundColor(.blue)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .aspectRatio(CGSize(width: 100, height: 30), contentMode: .fit)
+                        .padding(.vertical, 5.0)
+                    
+                        Text("Previous Record")
+                })
+                
+                ZStack(alignment: /*@START_MENU_TOKEN@*/Alignment(horizontal: .center, vertical: .center)/*@END_MENU_TOKEN@*/, content: {
+                        Rectangle()
+                            .foregroundColor(.blue)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .aspectRatio(CGSize(width: 100, height: 30), contentMode: .fit)
+                        .padding(.vertical, 5.0)
+                    
+                        Text("Next Record")
+                })
+            })
+                
+            Spacer()
             
             
-  
-         // Display standard footer
+            
+            // Display standard footer
             FooterView()
             
             
